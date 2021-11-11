6259b907c6030aa28f91db07221f37a579c9ef67
jainh-010 <84868532+jainh-010@users.noreply.github.com>
2021-11-11 11:33:07 +0530

    Update contratct.sol

--- contratct.sol
+++ contratct.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-
+//Tokenization of assest in fungible tool
 pragma solidity ^0.8.0;
 
 import "https://github.com/openzeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
